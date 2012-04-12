4b7e37c6f21f799d4bab0d9238d50f955f35282c
Kingston <kingsfung@gmail.com>
2012-04-11 17:54:47 -0700

    Adding db wrapper (limited)

---
+++ theory/db/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+##### System wide lib #####
+from mongoengine import *
+
+##### Theory lib #####
+
+##### Theory third-party lib #####
+
+##### Local app #####
+
+##### Theory app #####
+
+##### Misc #####
+
+# TODO: should add an abstract layer
