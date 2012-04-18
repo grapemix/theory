7e4d6a41d2ee59de5b7d3bd9b238d3a5f2f202ff
Kingston <kingsfung@gmail.com>
2012-04-18 09:14:56 -0700

    Adding test for theory

---
+++ tests/unitTest/core/tests/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+##### System wide lib #####
+
+##### Theory lib #####
+
+##### Theory third-party lib #####
+
+##### Local app #####
+#from .reactor import Reactor
+from reactor import *
+
+##### Theory app #####
+
+##### Misc #####
+
+
---
+++ theory/test/__init__.py
@@ -0,0 +1,6 @@
+"""
+Theory Unit Test and Doctest framework.
+"""
+
+from theory.test.testcases import (TestCase, SimpleTestCase)
+from theory.test.utils import Approximate
