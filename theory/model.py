01384c5afa657959024cb3781be68e55a3e2949c
Kingston <kingsfung@gmail.com>
2012-04-18 10:03:17 -0700

    Adding mulitple mood in a command in model level

--- theory/model.py
+++ theory/model.py
@@ -36,20 +36,28 @@ class Command(Document):
       verbose_name=_("Application"),\
       unique_with="name",\
       help_text=_("The applications which carry this command"))
-  mood = StringField(max_length=256,\
+  mood = ListField(StringField(max_length=256,\
       required=True,
       verbose_name=_("Mood"),\
-      help_text=_("The moods which carry this command"))
-  param = ListField(EmbeddedDocumentField(Parameter),\
+      help_text=_("The moods which carry this command")))
+  param = SortedListField(EmbeddedDocumentField(Parameter),\
       verbose_name=_("Parameter"),\
       help_text=_("The parameters of this command"))
   sourceFile = StringField(help_text=_("Command's source code location"))
   comment = StringField(help_text=_("Command's comment"))
+  # We have to do reverse_delete_rule=CASCADE) ourself to avoid inf loop
+  nextAvblCmd = ListField(ReferenceField("self"), \
+      verbose_name=_("Next available command"),\
+      help_text=_("The commands which are able to concatenate the result of this command"))
+
+  @property
+  def className(self):
+    return self.name[0].upper() + self.name[1:]
 
 class History(Document):
   command = StringField(required=True,\
-      verbose_name=_("Command"),\
-      help_text=_("Command being executer"))
+      verbose_name=_("Command in Text"),\
+      help_text=_("Command in Text including paramter"))
   commandRef = ReferenceField(Command, required=True,\
       reverse_delete_rule=CASCADE)
   mood = StringField(max_length=256,\
@@ -58,4 +66,18 @@ class History(Document):
       help_text=_("The moods where the command being executed"))
   touched = DateTimeField(required=True, default=datetime.utcnow())
   repeated = IntField(default=1)
+
+class Adapter(Document):
+  name = StringField(required=True, max_length=256,\
+      verbose_name=_("Adapter name"),\
+      help_text=_("Adapter name"))
+  importPath = StringField(max_length=256,\
+      required=True,
+      verbose_name=_("Import Path"),\
+      unique=True,\
+      help_text=_("The path to import the adapter"))
+  property = ListField(StringField(max_length=256,\
+      verbose_name=_("Property"),\
+      help_text=_("The properties which accepted by this adapter")))
+
 
