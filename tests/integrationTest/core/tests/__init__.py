8b94dba58d2090e89ce5c5df090b8eb082ba2eb1
Kingston <kingsfung@gmail.com>
2012-09-20 09:38:53 -0700

    1) Adding integration test 2) Adding bridge integration test

---
+++ tests/integrationTest/core/tests/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+##### System wide lib #####
+
+##### Theory lib #####
+
+##### Theory third-party lib #####
+
+##### Local app #####
+from bridge import *
+
+##### Theory app #####
+
+##### Misc #####
+
+
