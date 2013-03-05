a43ae27347e689fcabf3ffef360b5bc1ac1969a5
Kingston <kingsfung@gmail.com>
2013-03-04 16:51:51 -0800

    Update for collection hijack in mongoengine 0.8

--- theory/db/models.py
+++ theory/db/models.py
@@ -15,38 +15,13 @@ import sys
 
 ##### Misc #####
 
-class ModelMetaClass(TopLevelDocumentMetaclass):
-  def __new__(cls, name, bases, attrs):
-    super_new = super(ModelMetaClass, cls).__new__
-    if(name=="Model"):
-      attrs["__metaclass__"] = TopLevelDocumentMetaclass
-    new_class = super_new(cls, name, bases, attrs)
-    attrs["__metaclass__"] = ModelMetaClass
-
-    # Figure out the app_label by looking one level up.
-    # For 'django.contrib.sites.models', this would be 'sites'.
-    model_module = sys.modules[new_class.__module__]
-    app_label = model_module.__name__.split('.')[-2]
-
-    if(hasattr(new_class, "_meta")):
-      new_class._meta["collection"] = "%s_%s" % (app_label, new_class._meta["collection"])
-      new_class._meta["app_label"] = app_label
-
-    return new_class
-
 class Model(Document):
-  __metaclass__ = ModelMetaClass
-  #__metaclass__ = TopLevelDocumentMetaclass
+  __metaclass__ = TopLevelDocumentMetaclass
 
-  #meta = { "abstract": False }
-  #meta = { "abstract": True }
-  meta = {'allow_inheritance': True}
+  meta = { "abstract": True }
 
-  '''
-  meta = { "app_label": sys.modules[__module__].__name__.split('.')[-2] , "allow_inheritance": True}
-  @classmethod
-  def _get_collection_name(cls):
-    """Returns the collection name for this class.
-    """
-    return cls._meta.get('app_label', "theory") + cls._meta.get('collection', None)
-  '''
+  def __init__(self, *args, **kwargs):
+    self.meta = {}
+    self.meta["collection"] = "%s_%s" % (sys.modules[self.__module__].__name__.split('.')[-2], self._get_collection_name())
+    super(Model, self).__init__(*args, **kwargs)
+
