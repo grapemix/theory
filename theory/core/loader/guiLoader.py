07c9138d77048a4f837952b16892e050a4988999
Kingston <kingsfung@gmail.com>
2013-11-04 21:41:30 -0800

    1) Adding periodic check + notification on AdapterBuffer model 2) Adding control-alt-q to quit feature 3) Adding gevent spawn model

--- theory/core/loader/guiLoader.py
+++ theory/core/loader/guiLoader.py
@@ -1,21 +1,35 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 ##### System wide lib #####
+from datetime import datetime, timedelta
+import gevent
 import os
 os.environ.setdefault("CELERY_LOADER", "theory.core.loader.celeryLoader.CeleryLoader")
+import signal
 
 ##### Theory lib #####
 from theory.conf import settings
 from theory.core.reactor import *
-from theory.model import Command
+from theory.model import AdapterBuffer, Command
 
 ##### Theory third-party lib #####
+from theory.gui.gtk.notify import getNotify
 
 ##### Local app #####
 
 ##### Theory app #####
 
 ##### Misc #####
+
+def _chkAdapterBuffer():
+  while(True):
+    adapterBufferModelLst = AdapterBuffer.objects(
+        created__gt=datetime.now()-timedelta(minutes=1)
+        )
+    for m in adapterBufferModelLst:
+      getNotify(m)
+    gevent.sleep(60)
+  return False
 
 def wakeup(settings_mod, argv=None):
   if(settings.DEBUG or Command.objects.count()==0):
@@ -25,9 +39,11 @@ def wakeup(settings_mod, argv=None):
     from theory.utils.importlib import import_module
     for cmd in Command.objects.all():
       import_module(cmd.moduleImportPath)
-  #from theory.core import t
-  #reactor = Reactor()
-  #reactor.test()
-  reactor.ui.drawAll()
 
-
+  gevent.signal(signal.SIGQUIT, gevent.shutdown)
+  gevent.joinall(
+      [
+        gevent.spawn(reactor.ui.drawAll),
+        gevent.spawn(_chkAdapterBuffer),
+      ]
+  )
